c83a1f81a23fa7bad7d25f88252118e5a4aa035a
Jesuseme <oyakhilomejesuseme@gmail.com>
2021-04-08 01:25:06 +0100

    mobile responsiveness fixed

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    let toggle = document.querySelector("#toggle");
+    toggle.classList.toggle("active");
+    let menu = document.querySelector("span")
+    menu.classList.toggle("active");
+}
