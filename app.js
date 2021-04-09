f3920a03242531dc30605970a464f045674623ae
spykelion <ndilionel6@gmail.com>
2021-04-09 09:28:56 +0100

    finalized app.js hamburger actions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let menu = document.getElementById("menu");
 let open = document.getElementById("menu-open");
 let close = document.getElementById("menu-close");
+let item = document.getElementById("item");
 
 open.addEventListener('click', hideMenu);
 close.addEventListener('click', showMenu);
@@ -8,9 +9,11 @@ close.addEventListener('click', showMenu);
 function hideMenu(){
     open.style.display = 'none';
     close.style.display = 'block';
+    item.style.display = "flex";
 }
 
 function showMenu(){
     close.style.display = 'none';
     open.style.display = 'block';
+    item.style.display = "none";
 }
